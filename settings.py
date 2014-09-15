740ddf703b901f920c445df7e5842c354ec791e7
buckie <wjmartino@gmail.com>
2014-09-14 22:50:06 -0400

    got the new freezer working
    Signed-off-by: buckie <wjmartino@gmail.com>

--- settings.py
+++ settings.py
@@ -13,12 +13,8 @@ def parent_dir(path):
     '''Return the parent of a directory.'''
     return os.path.abspath(os.path.join(path, os.pardir))
 
-PROJECT_ROOT = os.path.join(parent_dir(APP_DIR), 'dist')
-# In order to deploy to Github pages, you must build the static files to
-# the project root
+PROJECT_ROOT = os.path.join(APP_DIR, 'build')
 FREEZER_DESTINATION = PROJECT_ROOT
-# Since this is a repo page (not a Github user page),
-# we need to set the BASE_URL to the correct url as per GH Pages' standards
 FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
                                     # will be deleted when you run the freezer
