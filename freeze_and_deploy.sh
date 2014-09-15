8a9ece36823afa596c2a9ab9a0a10e03ecb499ed
buckie <wjmartino@gmail.com>
2014-09-14 23:35:11 -0400

    minor update for testing purposes
    Signed-off-by: buckie <wjmartino@gmail.com>

--- freeze_and_deploy.sh
+++ freeze_and_deploy.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
-[ -r "./freeze.py" ] && python ./freeze.py && echo 'Site frozen' || echo 'Freezing issue, bailing!' && exit 1
-[ -d "./build" ] && git subtree push --prefix ./build origin master && echo 'Site Depolyed' || echo 'Deploy failure' && exit 1 
+# The classic -- three finger claw technique
+shout() { echo "$0: $*" >&2; }
+barf() { shout "$*"; exit 111; }
+safe() { "$@" || barf "cannot $*"; }
+
+if [ -r "./freeze.py" ]; then
+    echo 'Begining Freezing Process'
+    safe python ./freeze.py 
+else
+    barf 'freeze.py is missing!'
+fi
+
+
+if [ -d "./build" ]; then
+    echo 'committing new frozen files'
+    safe git add ./build/*
+    safe git commit -sm "updated frozen files"
+    
+    echo 'begining push to github'
+    safe git subtree push --prefix ./build origin master 
+else
+    barf 'Deploy failure, ./build does not exist'
+fi
+
+exit 0
+    
 
