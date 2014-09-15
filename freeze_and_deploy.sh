86325a88a5de43e957772f7dafd73153b8701193
buckie <wjmartino@gmail.com>
2014-09-14 23:08:33 -0400

    forgot that deploy comes from master and not gh-pages
    Signed-off-by: buckie <wjmartino@gmail.com>

--- freeze_and_deploy.sh
+++ freeze_and_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 [ -r "./freeze.py" ] && python ./freeze.py && echo 'Site frozen' || echo 'Freezing issue, bailing!' && exit 1
-[ -d "./build" ] && git subtree push --prefix ./build origin gh-pages && echo 'Site Depolyed' || echo 'Deploy failure' && exit 1 
+[ -d "./build" ] && git subtree push --prefix ./build origin master && echo 'Site Depolyed' || echo 'Deploy failure' && exit 1 
 
