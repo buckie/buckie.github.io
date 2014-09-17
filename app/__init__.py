f571c867ed50794951f8597463f3838c2162a9be
buckie <wjmartino@gmail.com>
2014-09-16 23:52:12 -0400

    got the compass system to work, not a huge fan though
    Signed-off-by: buckie <wjmartino@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_flatpages import FlatPages
 from flask_frozen import Freezer
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
+app.config.from_pyfile('config.py')
 pages = FlatPages(app)
 freezer = Freezer(app)
 
--- run.py
+++ run.py
@@ -1,8 +1,24 @@
-import os
+import subprocess32
 
 from app import app
 
 
+# Utility Functions
+def compass_compile(app_dir):
+    """
+    Use this to recompile sass into css
+    :param app_dir:
+    """
+    with subprocess32.Popen(['compass', 'compile'],
+                            stdout=subprocess32.PIPE, stderr=subprocess32.PIPE, cwd=app_dir) as p:
+        stdout, stderr = p.communicate()
+        if stdout or stderr:
+            print "-- COMPASS SAYS --", stderr.decode(errors='replace'), stdout.decode(errors='replace')
+        else:
+            print "No Compass Updates"
+
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    compass_compile(app.config['APP_DIR'])
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'],
+            extra_files=app.config['EXTRA_FILES'])
